59b6ecdc4a1240e63aeab6893df0d11a629f9499
Lorenc Estrefi <jlorenc1986@gmail.com>
2024-09-15 16:55:27 +0200

    adds docker

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,9 +6,22 @@ import { getDashboardData } from "./controllers/getDashboardData";
 import * as mockResponse from "./constants/mockApiResponse.json";
 import axios from "axios";
 import locale from "./constants/locale";
+import { Server } from "http";
+
+jest.setTimeout(3000);
 jest.mock("axios");
 
+let server: Server;
+
 describe("GET /", () => {
+  beforeEach(async () => {
+    server = await app.listen(4000);
+  });
+
+  afterEach(async () => {
+    return await server.close();
+  });
+
   it("renders home page with data from govData api", async () => {
     // mock api request
     const mock = axios as jest.Mocked<typeof axios>;
